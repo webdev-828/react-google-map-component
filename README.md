6f5142e4b918527607316afd7f802fdfba828c19
WebStudio523 <jame.bond0523@gmail.com>
2019-05-19 16:51:59 +0800

    master

---
+++ README.md
@@ -0,0 +1,13 @@
+# Datavis Storybook
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run storybook`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:9009](http://localhost:9009) to view it in the browser.
+
+
+
--- src/stories/index.js
+++ src/stories/index.js
@@ -8,18 +8,11 @@ import '../global.scss';
 
 // UI
 import AddPage from '../components/UI/AddPage';
-import Proportional from '../components/UI/MapviewDot'
+
 
 storiesOf('UI', module).addDecorator(StoryRouter()).addDecorator(withKnobs)
   .add('AddPage', () => (
     <AddPage
-    title={ text('Title',"Untitled")}
-    API={ text('API',"TTO7CfG")}
-    slug={ text('Slug',"https://libds.nus.edu.sg/arfandi_project/")}
-    />
-  ) )
-  .add('Proportional', () => (
-    <Proportional
     title={ text('Title',"Untitled")}
     API={ text('API',"TTO7CfG")}
     slug={ text('Slug',"https://libds.nus.edu.sg/arfandi_project/")}
